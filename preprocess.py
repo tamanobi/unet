edc3799689a83554e4f0554671a2dec27a31475b
tamanobi <tamanobi@gmail.com>
2021-06-23 23:22:09 +0900

    feat: glob

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,25 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
+from pathlib import Path
 
-path = "data/SegmentationClass/nude22_1.png"
-img = image.load_img(path)
-arr = image.img_to_array(img)
-white = np.array([0, 0, 0], dtype=np.uint8)
-one = np.array([1, 1, 1], dtype=np.uint8)
-print(arr.shape)
-print(arr[0, 0, :])
-output = arr.copy()
-valid = np.all(arr == [255,0,124], axis=-1)
-print(np.sum(valid))
-rs, cs = valid.nonzero()
-print(rs, cs)
-output[rs, cs, :] = [1, 1, 1]
-print(output[0, 0, :])
-white_img = image.array_to_img(output, scale=False)
-image.save_img("white_img.png", white_img)
+d = Path("data/SegmentationClass")
+out_d = Path("data/zero_one")
+if not out_d.exists():
+    out_d.mkdir()
+for path in d.glob("*.png"):
+    img = image.load_img(str(path))
+    arr = image.img_to_array(img)
+    white = np.array([0, 0, 0], dtype=np.uint8)
+    one = np.array([1, 1, 1], dtype=np.uint8)
+    print(arr.shape)
+    print(arr[0, 0, :])
+    output = arr.copy()
+    valid = np.all(arr == [255,0,124], axis=-1)
+    print(np.sum(valid))
+    rs, cs = valid.nonzero()
+    print(rs, cs)
+    output[rs, cs, :] = [1, 1, 1]
+    print(output[0, 0, :])
+    zero_one = image.array_to_img(output, scale=False)
+    image.save_img(str(out_d / d.name), zero_one)
