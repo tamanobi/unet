dc090fbd7005e3ef6ec4f8cc9241e27ed9804b72
tamanobi <tamanobi@gmail.com>
2021-06-25 03:46:03 +0900

    refactor: 整形

--- learn.py
+++ learn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from pathlib import Path
 from typing import Generator, Tuple, Protocol
+import numpy as np
 
 class DirPair(Protocol):
     IMAGE_DIR: str
@@ -25,14 +26,17 @@ def image_pairs(config: DirPair) -> Generator[Tuple[Path, Path], None, None]:
         yield path, mask
 
 
-for img, mask in image_pairs(config):
-    print(img, mask)
+def generator(datum: Generator[Tuple[Path, Path], None, None]) -> Generator[dict, None, None]:
+    """numpy 形式で画像を読み込む"""
+    for img, mask in datum:
+        base_img = image.load_img(str(img))
+        mask_img = image.load_img(str(mask), grayscale=True)
+        yield {
+            "image": image.img_to_array(base_img, dtype=np.uint8),
+            "mask": image.img_to_array(mask_img, dtype=np.uint8)
+        }
 
-
-def generator(datum):
-    for data in datum:
-        yield {data[key] for key in ["image", "segmentation_mask"]}
-
+generator(image_pairs(config))
 
 def normalize(input_image, input_mask):
   input_image = tf.cast(input_image, tf.float32) / 255.0
